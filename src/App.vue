55467dbdf356a87a90e3004f9249bd01f987215f
pivarcsi.gergely <gergely.pivarcsi@gmail.com>
2022-02-08 16:43:19 +0100

    1. feladat kész: betölti az adatbázis adatait

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,69 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+<table>
+      <thead>
+        <tr>
+          <th>Személy</th>
+          <th>Magasság</th>
+          <th>Ár</th>
+          <th>Műveletek</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="statue in statueList" v-bind:key="statue.id">
+          <td>{{ statue.person }}</td>
+          <td>{{ statue.height }}</td>
+          <td>{{ statue.price }}</td>
+          <td>
+            <button @click="deleteStatue(statue.id)">Törlés</button>
+            <button @click="editStatue(statue.id)">Szerkesztés</button>
+          </td>
+        </tr>
+
+        <tr>
+          <td>
+            <input type="hidden" v-model="statue.id">
+          </td>
+          <td>
+            <input type="text" v-model="statue.person">
+          </td>
+          <td>
+            <input type="number" v-model="statue.height">
+          </td>
+          <td>
+            <input type="number" v-model="statue.price">
+          </td>
+          <td>
+            <button @click="newStatue">Létrehoz</button>
+            <button @click="saveStatue">Mentés</button>
+          </td>
+        </tr>
+      </tbody>
+    </table>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "Statues",
+  data() {
+    return {
+      statue: {
+        id: null,
+        person: '',
+        height: null,
+        price: null
+      },
+      statueList:[]
+    }
+  },
+  methods: {
+    async loadData() {
+     let Response = await fetch('http://127.0.0.1:8000/api/statues')
+     let data = await Response.json()
+     this.statueList = data
+    },
+  },
+  mounted() {
+    this.loadData()
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
